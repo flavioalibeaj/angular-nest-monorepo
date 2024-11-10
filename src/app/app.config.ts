4baf38d8e4f161fb7c1df2fc6610d3474c73754e
alibeaj.flavio <alibeaj.flavio@gmail.com>
2024-11-10 23:21:15 +0100

    removed files, changed theme, added default configs for form fields

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,11 +3,31 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideAnimationsAsync(),
+    {
+      provide: MAT_FORM_FIELD_DEFAULT_OPTIONS,
+      useValue: {
+        appearance: 'outline',
+      },
+    },
+    // {
+    //   provide: MAT_SNACK_BAR_DEFAULT_OPTIONS,
+    //   useValue: {
+    //     duration: '4000',
+    //   },
+    // },
+    // {
+    //   provide: MAT_DIALOG_DEFAULT_OPTIONS,
+    //   useValue: {
+    //     autoFocus: false,
+    //     disableClose: true,
+    //   },
+    // }
   ],
 };
