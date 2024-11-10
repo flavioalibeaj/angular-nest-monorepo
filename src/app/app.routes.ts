0d66ce05405cba3c85cd7c638f5e7405161c0429
alibeaj.flavio <alibeaj.flavio@gmail.com>
2024-11-10 15:50:53 +0100

    set up main routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.routes').then((r) => r.authRoutes),
+  },
+  {
+    path: '',
+    loadComponent: () =>
+      import('./home/home.component').then((c) => c.HomeComponent),
+    loadChildren: () => import('./home/home.routes').then((r) => r.homeRoutes),
+  },
+  {
+    path: 'not-found',
+    loadComponent: () =>
+      import('./not-found/not-found.component').then(
+        (c) => c.NotFoundComponent
+      ),
+  },
+  {
+    path: '**',
+    redirectTo: 'not-found',
+    pathMatch: 'full',
+  },
+];
