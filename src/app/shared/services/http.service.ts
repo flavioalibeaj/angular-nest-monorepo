103bfb9381ffbf55d5b8235bf8e634313cb9213c
alibeaj.flavio <alibeaj.flavio@gmail.com>
2024-11-16 21:00:41 +0100

    added api response on http service

--- src/app/shared/services/http.service.ts
+++ src/app/shared/services/http.service.ts
@@ -2,6 +2,7 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { environment } from '../../../environments/environment.development';
 import { Observable } from 'rxjs';
+import { IApiResponse } from '../model/i-api-response.interface';
 
 @Injectable({
   providedIn: 'root',
@@ -10,27 +11,46 @@ export class HttpService {
   readonly #httpClient = inject(HttpClient);
   readonly #apiUrl = environment.apiUrl;
 
-  post<BT = unknown, RT = unknown>(url: string, body: BT): Observable<RT> {
-    return this.#httpClient.post<RT>(`${this.#apiUrl}/${url}`, body);
+  post<BT = unknown, RT = unknown>(
+    url: string,
+    body: BT
+  ): Observable<IApiResponse<RT>> {
+    return this.#httpClient.post<IApiResponse<RT>>(
+      `${this.#apiUrl}/${url}`,
+      body
+    );
   }
 
-  get<RT = unknown>(url: string, params?: HttpParams): Observable<RT> {
-    return this.#httpClient.get<RT>(`${this.#apiUrl}/${url}`, { params });
+  get<RT = unknown>(
+    url: string,
+    params?: HttpParams
+  ): Observable<IApiResponse<RT>> {
+    return this.#httpClient.get<IApiResponse<RT>>(`${this.#apiUrl}/${url}`, {
+      params,
+    });
   }
 
   getById<T = unknown, RT = unknown>(
     url: string,
     id: T,
     params?: HttpParams
-  ): Observable<RT> {
-    return this.#httpClient.get<RT>(`${this.#apiUrl}/${url}/${id}`, { params });
+  ): Observable<IApiResponse<RT>> {
+    return this.#httpClient.get<IApiResponse<RT>>(
+      `${this.#apiUrl}/${url}/${id}`,
+      { params }
+    );
   }
 
-  put<BT, RT = unknown>(url: string, body: BT): Observable<RT> {
-    return this.#httpClient.put<RT>(`${this.#apiUrl}/${url}`, body);
+  put<BT, RT = unknown>(url: string, body: BT): Observable<IApiResponse<RT>> {
+    return this.#httpClient.put<IApiResponse<RT>>(
+      `${this.#apiUrl}/${url}`,
+      body
+    );
   }
 
-  delete<T, RT = unknown>(url: string, id: T): Observable<RT> {
-    return this.#httpClient.delete<RT>(`${this.#apiUrl}/${url}/${id}`);
+  delete<T, RT = unknown>(url: string, id: T): Observable<IApiResponse<RT>> {
+    return this.#httpClient.delete<IApiResponse<RT>>(
+      `${this.#apiUrl}/${url}/${id}`
+    );
   }
 }
