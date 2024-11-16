368249b33f71627cd1148911d8bc91289eea944e
alibeaj.flavio <alibeaj.flavio@gmail.com>
2024-11-16 12:48:35 +0100

    http service

--- src/app/shared/services/http.service.ts
+++ src/app/shared/services/http.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { environment } from '../../../environments/environment.development';
 import { Observable } from 'rxjs';
@@ -10,7 +10,27 @@ export class HttpService {
   readonly #httpClient = inject(HttpClient);
   readonly #apiUrl = environment.apiUrl;
 
-  post<BT = any, RT = any>(url: string, body: BT): Observable<RT> {
+  post<BT = unknown, RT = unknown>(url: string, body: BT): Observable<RT> {
     return this.#httpClient.post<RT>(`${this.#apiUrl}/${url}`, body);
+  }
+
+  get<RT = unknown>(url: string, params?: HttpParams): Observable<RT> {
+    return this.#httpClient.get<RT>(`${this.#apiUrl}/${url}`, { params });
+  }
+
+  getById<T = unknown, RT = unknown>(
+    url: string,
+    id: T,
+    params?: HttpParams
+  ): Observable<RT> {
+    return this.#httpClient.get<RT>(`${this.#apiUrl}/${url}/${id}`, { params });
+  }
+
+  put<BT, RT = unknown>(url: string, body: BT): Observable<RT> {
+    return this.#httpClient.put<RT>(`${this.#apiUrl}/${url}`, body);
+  }
+
+  delete<T, RT = unknown>(url: string, id: T): Observable<RT> {
+    return this.#httpClient.delete<RT>(`${this.#apiUrl}/${url}/${id}`);
   }
 }
