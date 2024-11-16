f28693438dbbb494be30100b7ddd2b5a8411a9e9
alibeaj.flavio <alibeaj.flavio@gmail.com>
2024-11-16 21:24:29 +0100

    added endpoints

--- src/app/shared/endpoints/endpoints.ts
+++ src/app/shared/endpoints/endpoints.ts
@@ -2,3 +2,11 @@ export const AUTH_ENDPOINTS = {
   login: 'auth/login',
   register: 'auth/register',
 };
+
+export const USER_ENDPOINTS = {
+  get: 'user',
+};
+
+export const PROFILE_ENDPOINTS = {
+  createProfile: 'profile',
+};
