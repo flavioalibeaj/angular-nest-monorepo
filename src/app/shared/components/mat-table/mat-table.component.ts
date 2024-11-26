ee6beb1543cb4871ee956dfe853f1bdead53aa27
alibeaj.flavio <alibeaj.flavio@gmail.com>
2024-11-27 00:00:53 +0100

    row selection on table in progress

--- src/app/shared/components/mat-table/mat-table.component.ts
+++ src/app/shared/components/mat-table/mat-table.component.ts
@@ -25,6 +25,8 @@ import { MatCardModule } from '@angular/material/card';
 import { MatMenuModule } from '@angular/material/menu';
 import { IRowButton } from '../../model/i-row-button.interface';
 import { IMatTableColumn } from '../../model/i-mat-table-column';
+import { SelectionModel } from '@angular/cdk/collections';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 
 @Component({
   selector: 'app-mat-table',
@@ -39,14 +41,24 @@ import { IMatTableColumn } from '../../model/i-mat-table-column';
     MatTooltip,
     MatCardModule,
     MatMenuModule,
+    MatCheckboxModule,
   ],
   templateUrl: './mat-table.component.html',
   styleUrl: './mat-table.component.scss',
 })
 export class MatTableComponent<T> implements AfterViewInit {
   readonly columns = input.required<IMatTableColumn<T>[]>();
-  readonly displayedColumns = computed(() => this.columns().map((c) => c.key));
+  readonly displayedColumns = computed(() => {
+    const columns = this.columns().map((c) => c.key);
+    const isSelectable = this.selectable();
+
+    if (isSelectable) columns.unshift('checkboxSelection');
+    return columns;
+  });
   readonly paginationParams = input.required<PaginationParams>();
+  readonly selectable = input<boolean>(false);
+  readonly multiSelect = input<boolean>(true);
+  readonly initiallySelectedValues = input<T[]>([]);
   readonly paginator = viewChild<MatPaginator>(MatPaginator);
   readonly sort = viewChild<MatSort>(MatSort);
 
@@ -57,6 +69,16 @@ export class MatTableComponent<T> implements AfterViewInit {
   get dataSource(): MatTableDataSource<T> {
     return new MatTableDataSource<T>(this.#dataSource());
   }
+
+  readonly selectionModel = computed(() => {
+    const isSelectable = this.selectable();
+    return isSelectable
+      ? new SelectionModel<T>(
+          this.multiSelect(),
+          this.initiallySelectedValues()
+        )
+      : undefined;
+  });
 
   readonly emptyData = new MatTableDataSource<T>([{ empty: 'row' } as T]);
 
@@ -97,5 +119,19 @@ export class MatTableComponent<T> implements AfterViewInit {
   onButtonClick(button: IRowButton<T>, element: T): void {
     if (this.isButtonDisabled(button, element)) return;
     button.clickEvent(element);
+  }
+
+  isAllSelected() {
+    const numSelected = this.selectionModel()?.selected.length;
+    const numRows = this.dataSource.data.length;
+    return numSelected === numRows;
+  }
+
+  masterToggle() {
+    this.isAllSelected()
+      ? this.selectionModel()?.clear()
+      : this.dataSource.data.forEach((row) =>
+          this.selectionModel()?.select(row)
+        );
   }
 }
