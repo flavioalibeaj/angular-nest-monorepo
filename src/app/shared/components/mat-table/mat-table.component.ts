8386cb83782c38e234e195e49368be1ef60124b2
alibeaj.flavio <alibeaj.flavio@gmail.com>
2024-11-29 00:15:58 +0100

    added filter, and printing ability

--- src/app/shared/components/mat-table/mat-table.component.ts
+++ src/app/shared/components/mat-table/mat-table.component.ts
@@ -35,6 +35,9 @@ import {
   trigger,
 } from '@angular/animations';
 import { NgTemplateOutlet } from '@angular/common';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-mat-table',
@@ -52,6 +55,9 @@ import { NgTemplateOutlet } from '@angular/common';
     MatCheckboxModule,
     MatProgressBarModule,
     NgTemplateOutlet,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule,
   ],
   templateUrl: './mat-table.component.html',
   styleUrl: './mat-table.component.scss',
@@ -100,12 +106,21 @@ export class MatTableComponent<T> implements AfterViewInit {
     return new MatTableDataSource<T>(this.#dataSource());
   }
 
+  readonly showFilter = input<boolean>();
+  readonly advancedFilter = input<boolean>(); // TODO add
+  readonly filterOnFront = input<boolean>();
+  readonly printAndExport = input<boolean>();
+  readonly printAndExportTitle = input<string>();
+
   readonly paginator = viewChild<MatPaginator>(MatPaginator);
   readonly sort = viewChild<MatSort>(MatSort);
 
   readonly pageChange = output<PageEvent>();
   readonly sortChange = output<Sort>();
   readonly toggleChange = output<T[]>();
+  readonly searchFilter = output<string>();
+
+  readonly searchFilterCtrl = new FormControl<string>('');
 
   ngAfterViewInit() {
     this.dataSource.paginator = this.paginator() ?? null;
@@ -170,5 +185,88 @@ export class MatTableComponent<T> implements AfterViewInit {
     event?.stopPropagation();
     this.expandedElement =
       this.expandedElement === element ? undefined : element;
+  }
+
+  filterData() {
+    const searchValue = this.searchFilterCtrl.value?.toLowerCase() ?? '';
+    this.searchFilter.emit(searchValue);
+  }
+
+  print() {
+    // TODO fix css borders
+    const printContent = this.#generateRows();
+    this.#openWindowPopup(printContent);
+  }
+
+  #generateRows(): string {
+    const filteredColumns = this.columns().filter((col) => !col.actions);
+
+    const tableHeaders = filteredColumns
+      .map((column) => `<th>${column.header}</th>`)
+      .join('');
+
+    let tableRows: string = '';
+
+    this.dataSource.data.forEach((item: any) => {
+      const row = `
+      <tr>
+        ${filteredColumns
+          .map((column) => {
+            const cellValue = column.cell
+              ? column.cell(item)
+              : item[column.key];
+            return `<td>${cellValue ?? '--'}</td>`;
+          })
+          .join('')}
+      </tr>
+    `;
+      tableRows += row;
+    });
+
+    const printContent = `
+      <table>
+        <thead>
+          <tr>${tableHeaders}</tr>
+        </thead>
+        <tbody>${tableRows}</tbody>
+      </table>
+    `;
+
+    return printContent;
+  }
+
+  #openWindowPopup(printContent: string) {
+    const popUpWin = window.open('', 'top=0,left=0,height=100%');
+    popUpWin?.document.write(`
+      <html lang="en">
+        <head>
+          <meta charset="UTF-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <title>${this.printAndExportTitle()}</title>
+          <style>
+              body {
+                font-family: arial;
+              }
+              table {
+                  width: 100%;
+                  border-collapse: collapse;
+                  border-radius: 5px;
+              }
+              th, td {
+                  border: 0.5px solid #b3b3b3;
+                  padding: 8px;
+                  text-align: center;
+              }
+              th {
+                  background-color: #f2f2f2;
+              }
+          </style>
+        </head>
+        <body onload="window.print();setTimeout(window.close, 0);">
+          <h1>${this.printAndExportTitle()}</h1>
+          ${printContent}
+        </body>
+      </html>`);
+    popUpWin?.document.close();
   }
 }
