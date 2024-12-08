a6bb20f8164d0d8f44fd2b76afc00564af6bd111
alibeaj.flavio <alibeaj.flavio@gmail.com>
2024-12-08 23:12:43 +0100

    added route and time on error handler

--- src/app/core/error-handler/global-error-handler.service.ts
+++ src/app/core/error-handler/global-error-handler.service.ts
@@ -1,15 +1,30 @@
 import { ErrorHandler, inject, Injectable } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
+import { take } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class GlobalErrorHandlerService implements ErrorHandler {
   readonly #snackbar = inject(MatSnackBar);
+  readonly #router = inject(Router);
+  readonly #translate = inject(TranslateService);
 
   handleError(error: Error): void {
-    console.log(error);
+    const message = error.message || 'Unknown error';
+    console.log({
+      message: message,
+      route: this.#router.url,
+      time: new Date().toISOString(),
+    });
 
-    this.#snackbar.open(error.message, 'Close');
+    this.#translate
+      .stream('GENERAL.CLOSE')
+      .pipe(take(1))
+      .subscribe((close) => {
+        this.#snackbar.open(message, close);
+      });
   }
 }
