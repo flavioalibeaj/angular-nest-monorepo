ad6a763a3a124b76b4c33f2d7fb3f02f30cc31aa
alibeaj.flavio <alibeaj.flavio@gmail.com>
2025-02-01 23:16:25 +0100

    feat: add dashboard and settings components with basic templates; update sidenav service with menu elements

---
+++ src/app/home/pages/dashboard/dashboard.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard',
+  imports: [],
+  template: `<p>dashboard works!</p> `,
+})
+export class DashboardComponent {}
--- src/app/home/services/sidenav.service.ts
+++ src/app/home/services/sidenav.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Observable, Subject } from 'rxjs';
+import { IMenuElement } from '../model/i-menu-element.interface';
 
 @Injectable({
   providedIn: 'root',
@@ -10,6 +11,11 @@ export class SidenavService {
   get toggle$(): Observable<void> {
     return this.#toggleSubject.asObservable();
   }
+
+  readonly menuElements: readonly IMenuElement[] = [
+    { name: 'PAGES.dashboard', url: 'dashboard', icon: 'dashboard' },
+    { name: 'PAGES.settings', url: 'settings', icon: 'gear' },
+  ];
 
   toggleSidenav(): void {
     this.#toggleSubject.next();
